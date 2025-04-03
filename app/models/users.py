37be319544285644b5bcb7c4fe5688098865c5fb
john-otienoh <johnteclaire@gmail.com>
2025-04-03 21:25:26 +0300

    Create and connect PostgreSQL database

---
+++ app/models/users.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from enum import Enum
+from sqlalchemy import (
+    Boolean,
+    Column,
+    DateTime,
+    Integer,
+    String,
+    func,
+    ForeignKey,
+    Enum as SQLEnum,
+)
+from app.database import Base
+from sqlalchemy.orm import mapped_column, relationship
+
+
+class User(Base):
+    class Roles(str, Enum):
+        DOCTOR = "Doctor"
+        PATIENT = "Patient"
+
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(150))
+    email = Column(String(255), unique=True, index=True)
+    password = Column(String(100))
+    is_active = Column(Boolean, default=False)
+    verified_at = Column(DateTime, nullable=True, default=None)
+    updated_at = Column(DateTime, nullable=True, default=None, onupdate=datetime.now)
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
+
+
+#     tokens = relationship("UserToken", back_populates="user")
+
+#     def get_context_string(self, context: str):
+#         return f"{context}{self.password[-6:]}{self.updated_at.strftime('%m%d%Y%H%M%S')}".strip()
+
+
+# class UserToken(Base):
+#     __tablename__ = "user_tokens"
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+#     user_id = mapped_column(ForeignKey('users.id'))
+#     access_key = Column(String(250), nullable=True, index=True, default=None)
+#     refresh_key = Column(String(250), nullable=True, index=True, default=None)
+#     created_at = Column(DateTime, nullable=False, server_default=func.now())
+#     expires_at = Column(DateTime, nullable=False)
+
+#     user = relationship("User", back_populates="tokens")
